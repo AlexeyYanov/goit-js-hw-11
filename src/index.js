507e5196a4ff8b4ef982f4a48ac7392f32c43c84
AlexeyYanov <250138@gmail.com>
2023-03-31 13:01:58 +0300

    avec les fautes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
+import { Axios } from 'axios';
+// import { refs } from './js/refs';
+const form = document.querySelector('.search-form');
+const loadMoreButton = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+console.log(form);
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  loadMoreButton: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
+};
